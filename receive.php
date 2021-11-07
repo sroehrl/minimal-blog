52a63a13a4c62f7a1db45a592614eab02ff73033
neoan <stefan.roehrl@gmail.com>
2021-11-07 00:07:22 -0400

    auto-log

--- receive.php
+++ receive.php
@@ -5,8 +5,10 @@ header("Content-Type: application/json");
 if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
     header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 }
+
 $data = file_get_contents('php://input');
 if ($data) {
+    file_put_contents('log.json', $data);
     $converted = json_decode($data, true);
     $fileName = 'articles/' . $converted['payload']['slug'] . '.json';
     if ($converted['event'] === 'deleted') {
